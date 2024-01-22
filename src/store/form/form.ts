ea759ea9e2bcbf4eb03825174e766e33b37b79ae
ermakov.dmitry <dmitrij.ermakov@convergent.digital>
2024-01-22 21:48:37 +0300

    refactored reducers

--- src/store/form/form.ts
+++ src/store/form/form.ts
@@ -20,11 +20,8 @@ export const form = createSlice({
         (group) => group.id !== action.payload,
       );
       const result = calculateTotalSum(handleGroupDelete);
-      return {
-        ...state,
-        groups: result.groups,
-        sum: result.sum,
-      };
+      state.groups = result.groups;
+      state.sum = result.sum;
     },
     deleteSubGroup: (state, action: PayloadAction<number | string>) => {
       const handleSubGroupDelete = state.groups.map((group) => ({
@@ -34,11 +31,8 @@ export const form = createSlice({
         ),
       }));
       const result = calculateTotalSum(handleSubGroupDelete);
-      return {
-        ...state,
-        groups: result.groups,
-        sum: result.sum,
-      };
+      state.groups = result.groups;
+      state.sum = result.sum;
     },
     deleteProduct: (state, action: PayloadAction<number | string>) => {
       const handleProductDelete = state.groups.map((group) => ({
@@ -51,85 +45,64 @@ export const form = createSlice({
         })),
       }));
       const result = calculateTotalSum(handleProductDelete);
-      return {
-        ...state,
-        groups: result.groups,
-        sum: result.sum,
-      };
+      state.groups = result.groups;
+      state.sum = result.sum;
     },
     addGroup: (state) => {
-      return {
-        ...state,
-        groups: [
-          ...state.groups,
-          {
-            id: new Date().getTime(),
-            sum: 0,
-            subGroups: [],
-          },
-        ],
-      };
+      state.groups.push({
+        id: new Date().getTime(),
+        sum: 0,
+        subGroups: [],
+      });
     },
     addSubGroup: (state, action: PayloadAction<number | string>) => {
-      return {
-        ...state,
-        groups: state.groups.map((group) => {
-          if (group.id === action.payload) {
+      state.groups = state.groups.map((group) => {
+        if (group.id === action.payload) {
+          return {
+            ...group,
+            subGroups: [
+              ...group.subGroups,
+              {
+                id: new Date().getTime(),
+                sum: 0,
+                products: [],
+              },
+            ],
+          };
+        }
+        return group;
+      });
+    },
+    addProduct: (state, action: PayloadAction<number | string>) => {
+      state.groups = state.groups.map((group) => ({
+        ...group,
+        subGroups: group.subGroups.map((subGroup) => {
+          if (subGroup.id === action.payload) {
             return {
-              ...group,
-              subGroups: [
-                ...group.subGroups,
+              ...subGroup,
+              products: [
+                ...subGroup.products,
                 {
                   id: new Date().getTime(),
+                  name: `Продукт ${subGroup.products.length + 1}`,
                   sum: 0,
-                  products: [],
+                  count: 0,
+                  price: 0,
                 },
               ],
             };
           }
-          return group;
+          return subGroup;
         }),
-      };
-    },
-    addProduct: (state, action: PayloadAction<number | string>) => {
-      return {
-        ...state,
-        groups: state.groups.map((group) => ({
-          ...group,
-          subGroups: group.subGroups.map((subGroup) => {
-            if (subGroup.id === action.payload) {
-              return {
-                ...subGroup,
-                products: [
-                  ...subGroup.products,
-                  {
-                    id: new Date().getTime(),
-                    name: `Продукт ${subGroup.products.length + 1}`,
-                    sum: 0,
-                    count: 0,
-                    price: 0,
-                  },
-                ],
-              };
-            }
-            return subGroup;
-          }),
-        })),
-      };
+      }));
     },
     setCount: (state, action: PayloadAction<IProductSum>) => {
       const calcSumResult = calculateTotalSum(state.groups, action.payload);
-      return {
-        ...state,
-        groups: calcSumResult.groups,
-        sum: calcSumResult.sum,
-      };
+      state.groups = calcSumResult.groups;
+      state.sum = calcSumResult.sum;
     },
     handleSubmissionState: (state) => {
-      return {
-        ...state,
-        isSubmitted: !state.isSubmitted,
-      };
+      state.isSubmitted = !state.isSubmitted;
     },
     setLocalStorage: (state) => {
       if (state.isSubmitted) {
@@ -148,11 +121,8 @@ export const form = createSlice({
       if (hasFormValue) {
         const decodedValue = decodeURIComponent(atob(hasFormValue));
         const {groups, sum} = JSON.parse(decodedValue) as IForm;
-        return {
-          ...state,
-          groups,
-          sum,
-        };
+        state.groups = groups;
+        state.sum = sum;
       }
     },
   },
